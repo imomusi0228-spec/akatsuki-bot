b3d605839d5a244aed34319f8af1731131cb94a1
imomusi0228imomusi0228-spec <imomusi0228@gmail.com>
2026-02-12 11:12:50 +0900

    Cleanup: Remove all diagnostic scripts and temporary log files

--- debug_tiers_standalone.js
+++
@@ -1,8 +0,0 @@
-import { TIERS, TIER_NAMES } from './core/tiers.js';
-
-console.log('TIERS:', TIERS);
-console.log('TIER_NAMES keys:', Object.keys(TIER_NAMES));
-console.log('Lookup 0:', TIER_NAMES[0]);
-console.log('Lookup 1:', TIER_NAMES[1]);
-console.log('Lookup "0":', TIER_NAMES['0']);
-console.log('Lookup "1":', TIER_NAMES['1']);
