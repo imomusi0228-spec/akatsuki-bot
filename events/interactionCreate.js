7d228348365c5c300e81aa4fb1047ff3af516bac
imomusi0228imomusi0228-spec <imomusi0228@gmail.com>
2026-02-25 19:13:51 +0900

    fix: ROOT_DIR error

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const ROOT_DIR = path.resolve(__dirname, ".."); // ← ★ここに追加
 
 export default {
     name: Events.InteractionCreate,
